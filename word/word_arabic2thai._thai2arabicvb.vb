a1fe55e6b4887a1fe72e4d4c6e660d9f9cf43ea8
prasertcbs <bluecotton.k@gmail.com>
2021-09-17 16:29:47 +0700

    Add files via upload

--- word/word_arabic2thai._thai2arabicvb.vb
+++ word/word_arabic2thai._thai2arabicvb.vb
@@ -1,6 +1,8 @@
-' Convert Thai numerals to Arabic and vice versa
+' Microsoft Word Macro
+' convert Thai numerals to Arabic numerals and vice versa
 ' Author: Prasert Kanawattanachai
-' prasert@cbs.chula.ac.th
+' email: prasert@cbs.chula.ac.th
+' last upadted: 2021-09-17
 
 Option Explicit
 
@@ -10,8 +12,6 @@ Sub arabic2thai()
     Selection.Find.Replacement.ClearFormatting
     Dim i As Integer
     For i = 0 To 9
-        'Debug.Print Chr(i)
-        'Debug.Print ChrW(3664 - 48 + i)
         With Selection.Find
             .Text = ChrW(48 + i) ' find 0-9 arabic numerals
             .Replacement.Text = ChrW(3664 + i)
@@ -37,7 +37,7 @@ Sub thai2arabic()
     Selection.Find.ClearFormatting
     Selection.Find.Replacement.ClearFormatting
     Dim i As Integer
-    For i = 0 To 9 
+    For i = 0 To 9
         With Selection.Find
             .Text = ChrW(3664 + i) ' find 0-9 Thai numerals
             .Replacement.Text = ChrW(48 + i)
@@ -57,4 +57,3 @@ Sub thai2arabic()
         Selection.Find.Execute Replace:=wdReplaceAll
     Next
 End Sub
-
